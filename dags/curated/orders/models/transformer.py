6f3c9e043dbd13ffb8fb722f3612c29a7f01374a
slafaurie <selafaurie@gmail.com>
2022-04-21 20:45:01 +0200

    Finish orders etl

--- dags/curated/orders/models/transformer.py
+++ dags/curated/orders/models/transformer.py
@@ -1,10 +1,14 @@
 import pandas as pd
-import numpy as np
 import logging
 
-class OrderCuratedTransformer:
+class OrdersTransformer:
     _logger = logging.getLogger(__name__)
 
-
-    def join():
-        pass
+    def curate_orders(orders_lean: pd.DataFrame, orders_ranking: pd.DataFrame) -> pd.DataFrame:
+        """
+        Return the curated dataframe. At this point, only a join is required between the sources
+        """
+        return (
+            orders_lean
+            .merge(orders_ranking, how="left")
+        )
